be7ab82cf357e794fad74c0d5b349340426cb926
TheSecondBread <gamezone2904@gmail.com>
2024-05-28 12:01:38 +0530

    projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Project({name,tech}) {
+export default function Project({name,tech,git,live}) {
   console.log(tech)
   
   return (
@@ -9,20 +9,24 @@ export default function Project({name,tech}) {
         style={{ width: "380px", height: "250px"}}
       >
         <div style={{gap:"15px",display:"flex",marginTop:"1rem",marginLeft:"18rem"}}>
+          <a href={git} target="_blank"> 
           <img
             src="/github-mark-white.png"
             style={{ width: "28px", height: "28px" }}
           />
+          </a>
+          <a href={live} target="_blank">
           <img
             src="/icons8-next-50.png"
-            style={{ width: "27px", height: "29px" }}
+            style={{ width: "27px", height: "29px",zIndex:"1000px" }}
           />
+          </a>
         </div>
         <div style={{marginTop:"3rem"}}>
           <h3>{name}</h3>
           <div style={{display:"flex",justifyContent:"center",gap:"1rem",fontFamily:"roboto"}}>
           {tech != undefined ? tech.map((b)=>(
-             <h5 key={b}>{b}</h5>
+             <h4 key={b}>{b}</h4>
           )): <h3>CURRENT</h3>
           }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,9 +10,9 @@ export default function Projects() {
       <div
         className="cards"
       >
-        <Project name={"REAL ESTATE"}></Project>
-        <Project name={"PORTFOLIO"}  tech={["HTML","CSS","REACT"]}></Project>
-        <Project name={"URL SHORTENER"} tech={["REACT","JAVA SPRING","POSTGRESQL"]}></Project>
+        <Project name={"REAL ESTATE"} tech={["REACT","MongoDB","NODE"]} git={"https://github.com/TheSecondBread/RentEase"} live={"https://rent-ease-gamma.vercel.app/"}></Project>
+        <Project name={"PORTFOLIO"}  tech={["HTML","CSS","REACT"]} git={"https://github.com/TheSecondBread/portfolio"} live={"https://pragnan.vercel.app/"}></Project>
+        <Project name={"URL SHORTENER"} tech={["REACT","JAVA SPRING","POSTGRESQL"]} git={"https://github.com/TheSecondBread/url-shortener-frontend"} live={"https://url-shortener-frontend-tau.vercel.app/"}></Project>
         
       </div>
     </div>
